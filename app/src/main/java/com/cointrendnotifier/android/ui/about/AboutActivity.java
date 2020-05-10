f3b14bce956c3e356ab32b8b3d78c942aa720a99
YardenShoham <hrsi88@gmail.com>
2020-05-10 09:10:12 +0300

    :robot: Make links clickable

--- app/src/main/java/com/cointrendnotifier/android/ui/about/AboutActivity.java
+++ app/src/main/java/com/cointrendnotifier/android/ui/about/AboutActivity.java
@@ -1,6 +1,8 @@
 package com.cointrendnotifier.android.ui.about;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,7 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        TextView view = (TextView) findViewById(R.id.about_content);
+        view.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
