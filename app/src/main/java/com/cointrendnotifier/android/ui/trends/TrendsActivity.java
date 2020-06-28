4d2be19cf65f20ab584c8a4f7fabc27f30998f03
Aviel212 <Aviel9731@gmail.com>
2020-06-29 00:17:34 +0300

    :sparkles: Add Trends Activity

--- app/src/main/java/com/cointrendnotifier/android/ui/trends/TrendsActivity.java
+++ app/src/main/java/com/cointrendnotifier/android/ui/trends/TrendsActivity.java
@@ -1,27 +1,23 @@
 package com.cointrendnotifier.android.ui.trends;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.beardedhen.androidbootstrap.AwesomeTextView;
-import com.beardedhen.androidbootstrap.BootstrapButton;
-import com.beardedhen.androidbootstrap.api.defaults.ButtonMode;
+import com.beardedhen.androidbootstrap.BootstrapLabel;
 import com.beardedhen.androidbootstrap.api.defaults.DefaultBootstrapBrand;
-import com.beardedhen.androidbootstrap.api.defaults.DefaultBootstrapSize;
 import com.beardedhen.androidbootstrap.font.FontAwesome;
 import com.cointrendnotifier.android.R;
 import com.cointrendnotifier.android.api.Events;
@@ -74,35 +70,6 @@ public class TrendsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trends);
 
-        /*
-        final TableLayout trendsTableBody = (TableLayout) findViewById(R.id.trends_table_body);
-
-        BootstrapButton moreButton = new BootstrapButton(this);
-        TableRow.LayoutParams moreButton_lp = new TableRow.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        moreButton.setLayoutParams(moreButton_lp);
-        moreButton.setBootstrapBrand(DefaultBootstrapBrand.PRIMARY);
-        moreButton.setBootstrapSize(DefaultBootstrapSize.SM);
-        moreButton.setButtonMode(ButtonMode.REGULAR);
-        moreButton.setRounded(true);
-        moreButton.setShowOutline(false);
-        moreButton.setText("More");
-        moreButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(TrendsActivity.this, TrendActivity.class);
-                intent.putExtra("id", "5e9b4f6a48cdb0002ab99f78"); // TODO - needs to be dynamically
-                TrendsActivity.this.startActivity(intent);
-            }
-        });
-
-        TableRow eventView = new TableRow(TrendsActivity.this);
-        eventView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
-        eventView.setWeightSum(100);
-
-        eventView.addView(moreButton);
-        trendsTableBody.addView(eventView);*/
 
     }
 
@@ -112,151 +79,144 @@ public class TrendsActivity extends AppCompatActivity {
         render();
     }
 
+    private LinearLayout createSpace() {
+        LinearLayout space = new LinearLayout(TrendsActivity.this);
+        space.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                5
+        ));
+        space.setOrientation(LinearLayout.HORIZONTAL);
+        return space;
+    }
+
+    @SuppressLint("SetTextI18n")
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
-    private TableRow createStaticEvent(final EventDto event) {
-        // Row container
-        TableRow eventView = new TableRow(TrendsActivity.this);
-        eventView.setLayoutParams(new TableLayout.LayoutParams(
-                TableLayout.LayoutParams.MATCH_PARENT,
-                TableLayout.LayoutParams.WRAP_CONTENT));
+    private LinearLayout createStaticEvent(final EventDto event, int i) {
+        // Table row
+        LinearLayout eventView = new LinearLayout(TrendsActivity.this);
+        eventView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                85
+        ));
+        eventView.setOrientation(LinearLayout.HORIZONTAL);
         eventView.setWeightSum(100);
+        eventView.setBackgroundResource(R.drawable.border);
 
-        // RowIndex container
-        LinearLayout rowIndexContainer = new LinearLayout(TrendsActivity.this);
-        TableRow.LayoutParams rowIndexContainer_lp = new TableRow.LayoutParams(
-                0, LinearLayout.LayoutParams.WRAP_CONTENT);
-        rowIndexContainer_lp.weight = 5;
-        rowIndexContainer.setLayoutParams(rowIndexContainer_lp);
-        rowIndexContainer.setBackgroundResource(R.drawable.border);
-
-        // Row Index
-        AwesomeTextView rowIndex = new AwesomeTextView(TrendsActivity.this);
+        BootstrapLabel rowIndex = new BootstrapLabel(this);
         LinearLayout.LayoutParams rowIndex_lp = new LinearLayout.LayoutParams(
-                TableRow.LayoutParams.MATCH_PARENT, 85);
-        rowIndex.setLayoutParams(rowIndex_lp);
+                0, ViewGroup.LayoutParams.MATCH_PARENT, 10f);
         rowIndex.setGravity(Gravity.CENTER_HORIZONTAL);
         rowIndex.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        rowIndex.setMarkdownText("1"); // TODO - needs to be dynamically
+        rowIndex.setLayoutParams(rowIndex_lp);
+        rowIndex.setText(i + ""); // TODO - needs to be dynamically
         rowIndex.setTextSize(R.dimen.bootstrap_h4_text_size);
-        rowIndex.setMinWidth(70);
-        rowIndex.setMaxHeight(100);
+        rowIndex.setBootstrapBrand(event.getProbability() > 0 ?
+                DefaultBootstrapBrand.SUCCESS :
+                DefaultBootstrapBrand.DANGER);
 
-        rowIndexContainer.addView(rowIndex);
+        eventView.addView(rowIndex);
 
         // ExpectedTo container
         LinearLayout expectedToContainer = new LinearLayout(TrendsActivity.this);
-        TableRow.LayoutParams wrapContent1 = new TableRow.LayoutParams(
-                0, LinearLayout.LayoutParams.WRAP_CONTENT);
-        wrapContent1.weight = 30f;
+        LinearLayout.LayoutParams wrapContent1 = new LinearLayout.LayoutParams(
+                0, LinearLayout.LayoutParams.MATCH_PARENT);
+        wrapContent1.weight = 25f;
         expectedToContainer.setLayoutParams(wrapContent1);
+        expectedToContainer.setBackgroundResource(R.drawable.border);
 
         // ExpectedTo
         AwesomeTextView expectedTo = new AwesomeTextView(TrendsActivity.this);
         LinearLayout.LayoutParams expectedTo_lp = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, 85);
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         expectedTo.setLayoutParams(expectedTo_lp);
-        expectedTo.setRotation(45f); // TODO - needs to be dynamically
+        double prob = event.getProbability();
+        expectedTo.setRotation(prob > 0 ?
+                45f : 135f);
         expectedTo.setText("");
-        expectedTo.setBootstrapBrand(DefaultBootstrapBrand.SUCCESS);
+        expectedTo.setBootstrapBrand(
+                event.getProbability() > 0 ?
+                        DefaultBootstrapBrand.SUCCESS : DefaultBootstrapBrand.DANGER);
         expectedTo.setFontAwesomeIcon(FontAwesome.FA_ARROW_UP);
-        expectedTo.setMinWidth(423);
+
         expectedToContainer.addView(expectedTo);
+        eventView.addView(expectedToContainer);
 
-        // Asset container
-        LinearLayout assetContainer = new LinearLayout(TrendsActivity.this);
-        TableRow.LayoutParams assetContainer_lp = new TableRow.LayoutParams(
-                0, LinearLayout.LayoutParams.WRAP_CONTENT);
-        assetContainer_lp.weight = 25;
-        assetContainer.setLayoutParams(assetContainer_lp);
-        assetContainer.setBackgroundResource(R.drawable.border);
-
-        // Asset
-        AwesomeTextView asset = new AwesomeTextView(TrendsActivity.this);
-        LinearLayout.LayoutParams asset_lp = new TableRow.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, 85);
+        BootstrapLabel asset = new BootstrapLabel(this);
+        //TextView asset = new TextView(TrendsActivity.this);
+        LinearLayout.LayoutParams asset_lp = new LinearLayout.LayoutParams(
+                0, ViewGroup.LayoutParams.MATCH_PARENT, 25f);
         asset.setLayoutParams(asset_lp);
-        asset.setText("BTC/USDT"); // TODO - needs to be dynamically
+        asset.setText(String.format("%s/%s", event.getBaseAssetName(), event.getQuoteAssetName()));
         asset.setTextSize(R.dimen.bootstrap_h4_text_size);
         asset.setGravity(Gravity.CENTER_HORIZONTAL);
         asset.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        asset.setBootstrapBrand(event.getProbability() > 0 ?
+                DefaultBootstrapBrand.SUCCESS :
+                DefaultBootstrapBrand.DANGER);
+        eventView.addView(asset);
 
-        assetContainer.addView(asset);
-
-        // probability container
-        LinearLayout probabilityContainer = new LinearLayout(TrendsActivity.this);
-        TableRow.LayoutParams probabilityContainer_lp = new TableRow.LayoutParams(
-                0, LinearLayout.LayoutParams.WRAP_CONTENT);
-        assetContainer_lp.weight = 26;
-        assetContainer.setLayoutParams(assetContainer_lp);
-        assetContainer.setBackgroundResource(R.drawable.border);
-
-        // probability
-        TextView probability = new TextView(TrendsActivity.this);
+        BootstrapLabel probability = new BootstrapLabel(TrendsActivity.this);
         LinearLayout.LayoutParams probability_lp = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, 85);
+                0, LinearLayout.LayoutParams.MATCH_PARENT, 26f);
         probability.setLayoutParams(probability_lp);
-        probability.setText("10%"); // TODO - needs to be dynamically
+        probability.setText(Math.round(Math.abs(event.getProbability()) * 100) + "%");
         probability.setTextSize(R.dimen.bootstrap_h4_text_size);
         probability.setGravity(Gravity.CENTER_HORIZONTAL);
         probability.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        probability.setMinWidth(367);
+        probability.setBootstrapBrand(event.getProbability() > 0 ?
+                DefaultBootstrapBrand.SUCCESS :
+                DefaultBootstrapBrand.DANGER);
 
-        probabilityContainer.addView(probability);
+        eventView.addView(probability);
 
         // moreButton container
         LinearLayout moreButtonContainer = new LinearLayout(TrendsActivity.this);
-        TableRow.LayoutParams wrapContent4 = new TableRow.LayoutParams(
-                0, TableRow.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams wrapContent4 = new LinearLayout.LayoutParams(
+                0, LinearLayout.LayoutParams.MATCH_PARENT, 14f);
+        moreButtonContainer.setOrientation(LinearLayout.HORIZONTAL);
         moreButtonContainer.setLayoutParams(wrapContent4);
-        wrapContent4.weight = 14f;
+
         // moreButton
-
-
-        BootstrapButton moreButton = new BootstrapButton(TrendsActivity.this);
+        BootstrapLabel moreButton = new BootstrapLabel(TrendsActivity.this);
         LinearLayout.LayoutParams moreButton_lp = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, 85);
+                LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         moreButton.setLayoutParams(moreButton_lp);
         moreButton.setBootstrapBrand(DefaultBootstrapBrand.PRIMARY);
-        moreButton.setBootstrapSize(DefaultBootstrapSize.SM);
-        moreButton.setButtonMode(ButtonMode.REGULAR);
         moreButton.setRounded(true);
-        moreButton.setShowOutline(false);
         moreButton.setText("More");
         moreButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(TrendsActivity.this, TrendActivity.class);
-                intent.putExtra("id", "5e9b4f6a48cdb0002ab99f78"); // TODO - needs to be dynamically
+                intent.putExtra("id", event.get_id());
                 TrendsActivity.this.startActivity(intent);
             }
         });
 
         moreButtonContainer.addView(moreButton);
-
-        eventView.addView(rowIndexContainer);
-        eventView.addView(expectedToContainer);
-        eventView.addView(assetContainer);
-        eventView.addView(probabilityContainer);
         eventView.addView(moreButtonContainer);
 
         return eventView;
     }
 
     private void render() {
-        final TableLayout trendsTableBody = (TableLayout) findViewById(R.id.trends_table_body);
+        final LinearLayout trendsTableBody = (LinearLayout) findViewById(R.id.trends_table_body);
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    final List<EventDto> eventsDtos = Events.getEvents(15);
+                    final List<EventDto> eventsDtos = Events.getEvents(20);
                     runOnUiThread(new Runnable() {
+                        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
                         @Override
                         public void run() {
-                            // trendsTableBody.removeAllViews();
+                            trendsTableBody.removeAllViews();
+                            int i = 0;
                             for (EventDto event : eventsDtos) {
-                                trendsTableBody.addView(createStaticEvent(event));
+                                i++;
+                                trendsTableBody.addView(createStaticEvent(event, i));
+                                trendsTableBody.addView(createSpace());
                             }
-                            Log.d("Yarden", String.valueOf(trendsTableBody.getChildCount()));
                         }
                     });
                 } catch (IOException e) {
